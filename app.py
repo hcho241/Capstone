7551955f22e882907e9af075bd76b511375729fb
hcho241 <54073458+hcho241@users.noreply.github.com>
2023-01-26 19:03:46 +0000

    file upload

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ async def delete_account(account_number: str) -> None:
     account_service.delete_account(account_number)
 
 if __name__ == "__main__":
-   uvicorn.run("app:app",host="127.0.0.1",port=8080,reload=True) 
+   uvicorn.run("app:app",host="127.0.0.1",port=8000,reload=True) 
